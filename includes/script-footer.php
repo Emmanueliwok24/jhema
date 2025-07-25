3ddf0b61510cca2edc86662c9b9c144049bdcfd2
emmanuel iwok <iwokemmanuel49@gmail.com>
2025-07-25 13:15:04 +0100

    Remove index.html file, eliminating all associated HTML content and structure from the project.

---
+++ includes/script-footer.php
@@ -0,0 +1,13 @@
+    <!-- External JavaScripts -->
+    <script src="js/plugins/jquery.min.js"></script>
+    <script src="js/plugins/bootstrap.bundle.min.js"></script>
+    <script src="js/plugins/bootstrap-slider.min.js"></script>
+    <!-- <script src="js/plugins/jquery.waypoints.min.js"></script> -->
+    <!-- <script src="js/plugins/sticky.min.js"></script> -->
+    <script src="js/plugins/swiper.min.js"></script>
+    <script src="js/plugins/countdown.js"></script>
+
+    <!-- Footer Scripts -->
+    <script src="js/theme.js"></script>
+  </body>
+</html>
---
+++ includes/scroll.php
@@ -0,0 +1,6 @@
+<!-- Go To Top -->
+<div id="scrollTop" class="visually-hidden end-0"></div>
+
+<!-- Page Overlay -->
+<div class="page-overlay"></div>
+<!-- /.page-overlay -->

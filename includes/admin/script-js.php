b80eba663b9b5fc4719aa697399cc756be3d1813
emmanuel iwok <iwokemmanuel49@gmail.com>
2025-08-11 00:52:40 +0100

    Add SVG icons for pet and vegetable, enhance admin layout with new header, sidebar, and logout modal
    - Introduced new SVG icons for pet and vegetable in the admin section.
    - Created a new header layout in head.php with meta tags and stylesheets for better SEO and responsiveness.
    - Implemented a logout modal in logout.php to confirm user actions before logging out.
    - Developed a comprehensive page header in page-header.php for improved navigation and user experience.
    - Updated script-js.php to include necessary JavaScript files for functionality and interactivity.
    - Designed a new sidebar in sidebar.php with links to various admin functionalities for easier navigation.
    - Established a database connection in config.php for backend operations.
    - Added utility functions in functions.php for slug generation, price formatting, image uploads, and data fetching.

---
+++ includes/admin/script-js.php
@@ -0,0 +1,50 @@
+
+    <!-- latest js -->
+    <script src="admin/js/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="admin/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+    <!-- feather icon js -->
+    <script src="admin/js/icons/feather-icon/feather.min.js"></script>
+    <script src="admin/js/icons/feather-icon/feather-icon.js"></script>
+
+    <!-- scrollbar simplebar js -->
+    <script src="admin/js/scrollbar/simplebar.js"></script>
+    <script src="admin/js/scrollbar/custom.js"></script>
+
+    <!-- Sidebar jquery -->
+    <script src="admin/js/config.js"></script>
+
+    <!-- tooltip init js -->
+    <script src="admin/js/tooltip-init.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="admin/js/sidebar-menu.js"></script>
+    <script src="admin/js/notify/index.js"></script>
+
+    <!-- Apexchar js -->
+    <script src="admin/js/chart/apex-chart/apex-chart1.js"></script>
+    <script src="admin/js/chart/apex-chart/moment.min.js"></script>
+    <script src="admin/js/chart/apex-chart/apex-chart.js"></script>
+    <script src="admin/js/chart/apex-chart/stock-prices.js"></script>
+    <script src="admin/js/chart/apex-chart/chart-custom1.js"></script>
+
+
+    <!-- slick slider js -->
+    <script src="admin/js/slick.min.js"></script>
+    <script src="admin/js/custom-slick.js"></script>
+
+
+
+    <!-- ratio js -->
+    <script src="admin/js/ratio.js"></script>
+
+    <!-- sidebar effect -->
+    <script src="admin/js/sidebareffect.js"></script>
+
+    <!-- Theme js -->
+    <script src="admin/js/script.js"></script>
+</body>
+
+</html>
---
+++ includes/admin/sidebar.php
@@ -0,0 +1,174 @@
+   <!-- Page Sidebar Start-->
+            <div class="sidebar-wrapper ">
+                <div>
+                    <div class="logo-wrapper logo-wrapper-center">
+                        <a href="admin-dashboard.php" data-bs-original-title="" title="">
+                            <img class="img-fluid " style="max-width: 200px; filter: brightness(0) invert(1);" src="images/logo.svg" alt="logo">
+                        </a>
+                        <div class="back-btn">
+                            <i class="fa fa-angle-left"></i>
+                        </div>
+                        <div class="toggle-sidebar">
+                            <i class="ri-apps-line status_toggle middle sidebar-toggle"></i>
+                        </div>
+                    </div>
+
+                    <nav class="sidebar-main">
+                        <div class="left-arrow" id="left-arrow">
+                            <i data-feather="arrow-left"></i>
+                        </div>
+
+                        <div id="sidebar-menu">
+                            <ul class="sidebar-links " id="simple-bar">
+                                <li class="back-btn"></li>
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title link-nav" href="admin-dashboard.php">
+                                        <i class="ri-home-line"></i>
+                                        <span>Dashboard</span>
+                                    </a>
+                                </li>
+
+                                <li class="sidebar-list">
+                                    <a class="linear-icon-link sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-store-3-line"></i>
+                                        <span>Product</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="products.html">Prodcts</a>
+                                        </li>
+
+                                        <li>
+                                            <a href="add-new-product.php">Add New Products</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-user-3-line"></i>
+                                        <span>Users</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="all-users.html">All users</a>
+                                        </li>
+                                        <li>
+                                            <a href="add-new-user.html">Add new user</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-user-3-line"></i>
+                                        <span>Roles</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="role.html">All roles</a>
+                                        </li>
+                                        <li>
+                                            <a href="create-role.html">Create Role</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-archive-line"></i>
+                                        <span>Orders</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="order-list.html">Order List</a>
+                                        </li>
+                                        <li>
+                                            <a href="order-detail.html">Order Detail</a>
+                                        </li>
+                                        <li>
+                                            <a href="order-tracking.html">Order Tracking</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+
+                                <li class="sidebar-list">
+                                    <a class="linear-icon-link sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-price-tag-3-line"></i>
+                                        <span>Coupons</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="coupon-list.html">Coupon List</a>
+                                        </li>
+
+                                        <li>
+                                            <a href="create-coupon.html">Create Coupon</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title link-nav" href="product-review.html">
+                                        <i class="ri-star-line"></i>
+                                        <span>Product Review</span>
+                                    </a>
+                                </li>
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title link-nav" href="support-ticket.html">
+                                        <i class="ri-phone-line"></i>
+                                        <span>Support Ticket</span>
+                                    </a>
+                                </li>
+
+                                <li class="sidebar-list">
+                                    <a class="linear-icon-link sidebar-link sidebar-title" href="javascript:void(0)">
+                                        <i class="ri-settings-line"></i>
+                                        <span>Settings</span>
+                                    </a>
+                                    <ul class="sidebar-submenu">
+                                        <li>
+                                            <a href="profile-setting.html">Profile Setting</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+
+                                <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title link-nav" href="list-page.html">
+                                        <i class="ri-list-check"></i>
+                                        <span>List Page</span>
+                                    </a>
+                                </li>
+
+                                <li class="sidebar-list  " >
+                                     <a data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="sidebar-link sidebar-title link-nav" href="javascript:void(0)">
+                                        <i class="ri-logout-box-line"></i>
+                                        <span>Log out</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+
+                    </nav>
+
+\
+
+                </div>
+
+
+
+            </div>
+            <!-- Page Sidebar Ends-->
